deda6d605ce1decb9e2c816d2ae039f36d1ed882
Agung Juli Anda <agungjulianda440@gmail.com>
2023-02-08 16:15:32 +0700

    Commit for Log Wording

--- testCases/test_006_DeletAccount.py
+++ testCases/test_006_DeletAccount.py
@@ -53,7 +53,7 @@ class Test_004_ConnectNewAccounts:
             self.account1 = len(self.driver.find_elements(By.CLASS_NAME,"card"))
             if self.account1 == 1:
                 self.logger.error("***** Delete Connected Account Failed ")
-                self.logger.error("***** CONNECT ACCOUNT TESTDATA INVALID")
+                self.logger.error("***** Delete Connected Account Failde : Test Data Invalid ")
                 self.driver.close()
                 assert False
                 
